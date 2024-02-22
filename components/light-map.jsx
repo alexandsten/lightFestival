1442ea9d4f4e49b1d6de2f352c133bcbb8f66b04
Alex A <alex.andsten@gmail.com>
2024-02-22 15:25:07 +0100

    map start

--- components/light-map.jsx
+++ components/light-map.jsx
@@ -1,8 +1,39 @@
-import React from 'react';
-import MapView from 'react-native-maps';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, TextInput, Text, StyleSheet } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
 export default function LightMap() {
+  const [selectedMarker, setSelectedMarker] = useState(null);
+
+  const markers = [
+    {
+      title: 'Marker 1',
+      coordinate: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+      },
+    },
+    {
+        title: 'Marker 2',
+        coordinate: {
+          latitude: 37.75885,
+          longitude: -122.4384,
+        },
+      },
+      {
+        title: 'Marker 3',
+        coordinate: {
+          latitude: 37.78245,
+          longitude: -122.4344,
+        },
+      },
+    // Add more markers as needed
+  ];
+
+  const handleMarkerPress = (marker) => {
+    setSelectedMarker(marker);
+  };
+
   return (
     <View style={styles.container}>
       <MapView
@@ -13,7 +44,32 @@ export default function LightMap() {
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
-      />
+      >
+        {markers.map((marker, index) => (
+          <Marker
+            key={index}
+            coordinate={marker.coordinate}
+            title={marker.title}
+            description="Custom description here"
+            image={require('../img/marker2.png')}
+            onPress={() => handleMarkerPress(marker)}
+          />
+        ))}
+      </MapView>
+      {selectedMarker && (
+        <View style={styles.selectedMarkerContainer}>
+          <Text style={styles.selectedMarkerText}>
+            {`Selected Marker: ${selectedMarker.title}`}
+          </Text>
+        </View>
+      )}
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Type here..."
+          // Add any additional props or event handlers as needed
+        />
+      </View>
     </View>
   );
 }
@@ -27,5 +83,33 @@ const styles = StyleSheet.create({
   },
   map: {
     ...StyleSheet.absoluteFillObject,
+  },
+  inputContainer: {
+    position: 'absolute',
+    bottom: 20,
+    left: 20,
+    right: 20,
+    backgroundColor: 'rgba(28, 27, 31, 0.55)',
+    borderRadius: 15,
+  },
+  textInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    paddingLeft: 10,
+    color: 'white',
+    borderRadius: 15,
+  },
+  selectedMarkerContainer: {
+    position: 'absolute',
+    bottom: 80,
+    left: 20,
+    right: 20,
+    backgroundColor: 'rgba(28, 27, 31, 0.8)',
+    borderRadius: 15,
+    padding: 10,
+  },
+  selectedMarkerText: {
+    color: 'white',
   },
 });
