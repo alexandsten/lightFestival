e1149a55c8cf2f64fac2f0b8b5873e48e2d00516
Alex A <alex.andsten@gmail.com>
2024-09-04 17:22:25 +0200

    share pictures

--- components/artwork-drawer.jsx
+++ components/artwork-drawer.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, Button } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, useWindowDimensions, Platform, Linking, ScrollView, } from 'react-native';
 import { Animated } from 'react-native';
 import { styles } from './styles-drawer';
@@ -6,13 +6,15 @@ import { ImageBackground } from 'react-native';
 import HTML from 'react-native-render-html';
 import * as Font from 'expo-font';
 import { LinearGradient } from 'expo-linear-gradient';
+import * as Sharing from 'expo-sharing';
+import * as FileSystem from 'expo-file-system';
 
 Font.loadAsync({
   'AlfredSans-Regular': require('../assets/fonts/AlfredSans-Regular.ttf'),
 });
 
 
-export default function ArtWorkDrawer({selectedMarker, setSelectedMarker, isVisible, isReadMore, setReadMore, menuDrawer,  userLocation , setIsVisible}) {
+export default function ArtWorkDrawer({selectedMarker, setSelectedMarker, isVisible, isReadMore, setReadMore, menuDrawer,  userLocation , setIsVisible }) {
 
   const translateY = useRef(new Animated.Value(300)).current;
   const windowWidth = useWindowDimensions().width;
@@ -85,6 +87,28 @@ const toggleVisibility = () => {
     handleCloseButtonPress();
   };
 
+  const downloadAndShareImage = async () => {
+    const remoteUri = selectedMarker.picture; // Remote image URI
+    const localUri = FileSystem.documentDirectory + 'image.png'; // Local file path
+
+    try {
+      // Download the image to the local file system
+      const { uri } = await FileSystem.downloadAsync(remoteUri, localUri);
+      
+      // Check if sharing is available
+      if (!(await Sharing.isAvailableAsync())) {
+        Alert.alert('Sharing is not available on this device');
+        return;
+      }
+
+      // Share the downloaded image
+      await Sharing.shareAsync(uri);
+    } catch (error) {
+      console.error('Error downloading or sharing image:', error);
+      Alert.alert('Error', error.message);
+    }
+  };
+
   
 
   return (
@@ -150,8 +174,11 @@ const toggleVisibility = () => {
                 </ScrollView>
               </View>
             )}
+             <TouchableOpacity onPress={downloadAndShareImage} >
+            <Text style={styles.closeButtonText}>Share</Text>
+          </TouchableOpacity>
           </View>
-          
+         
           <TouchableOpacity onPress={readMore} style={styles.readMoreButton}>
             <Text style={styles.closeButtonText}>Expand</Text>
           </TouchableOpacity>
